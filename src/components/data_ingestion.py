8805a6673f9d81378749c1b7d5f81baca425f30f
raunaqmittal <raunaqmittal2004@gmail.com>
2026-01-13 00:18:24 +0530

    made data transformation file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,8 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -21,7 +22,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv(r'notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -48,7 +49,8 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-
-
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
